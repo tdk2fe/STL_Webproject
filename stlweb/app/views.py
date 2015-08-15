5b8134898f2b2134a8752c6a4a815e3325f5b13e
Tim Krupinski <tkrupinski@Falcon.attlocal.net>
2015-08-15 16:17:23 -0500

    added broke out main view and body blocks

--- stlweb/app/views.py
+++ stlweb/app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World"
+	user = {'nickname': 'Tim'}
+	posts = [
+		{
+			'alderman': {'name': 'Megan Green'},
+			'body': '15th ward'
+		},
+		{
+			'alderman': {'name': 'Antonia French'},
+			'body': '12th ward'
+		}
+	]
+	return render_template('index.html',
+		title='Home',
+		user=user,
+		posts=posts)
