6a41a4b8f150d2ef5df104ac88eb87a2c9ee2aab
Tim Krupinski <tdkrupinski@gmail.com>
2015-08-16 08:42:02 -0500

    Updated login form to support openid

--- stlweb/config.py
+++ stlweb/config.py
@@ -1,2 +1,8 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'local-secret'
+
+OPENID_PROVIDERS = [
+	{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+	{'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+	{'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
