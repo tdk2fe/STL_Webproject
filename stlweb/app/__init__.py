91df7128f9de907067ff463b695674cbe3823369
Tim Krupinski <tdkrupinski@gmail.com>
2015-08-18 13:17:43 -0500

    added gitignore

--- stlweb/app/__init__.py
+++ stlweb/app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-from app import views
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+db = SQLAlchemy(app)
+
+from app import views, models
